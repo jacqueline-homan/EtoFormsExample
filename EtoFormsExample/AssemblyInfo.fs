e8c1a14c2ff941610fd6a0f2e19c80cfac463d25
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-10-13 12:36:21 -0400

    First commit

---
+++ EtoFormsExample/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿namespace EtoFormsExample
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("EtoFormsExample")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Codecatenation, LLC")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Jacqueline S. Homan")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyPresentationFrameworkElement (culture="neutral", PublicKeyToken="31bf3856ad364a35") Version = 4.0.0.0)>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ EtoFormsExample/Program.fs
@@ -0,0 +1,22 @@
+﻿open Eto
+open Eto.Forms
+open Eto.Drawing
+open EtoUtils
+
+let app = new Application()
+
+let form = new Form(Title="Curves", TopMost=true, Size=Size(640, 480))
+let cpt = [|PointF(20.f, 60.f); PointF(40.f, 50.f); PointF(130.f, 60.f); PointF(200.f, 200.f)|]
+let mutable movingPoint = -1
+let draw = Drawable()
+let menuBezier = new CheckMenuItem(Text="Show &Bezier", Checked=true)
+let menuCanonical = new CheckMenuItem(Text="Show &Canonical spline")
+let menuControlPoints = new CheckMenuItem(Text="Show control &points")
+
+let tension = new Slider(Orientation=SliderOrientation.Vertical, MinValue=0, MaxValue=10, Visible-false)
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
